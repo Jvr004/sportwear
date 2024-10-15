d12ada3742935046bc9617b59f2731487b65c94f
Jvr <jvr.t004@gmail.com>
2024-10-15 10:40:43 -0300

    Cambios de url en  header footer

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -10,17 +10,17 @@ header.innerHTML = `
       </div>
       <div class="nav-links">
         <ul>
-          <li><a href="/index.html">Inicio</a></li>
+          <li><a href="index.html">Inicio</a></li>
           <li><a href="about-us.html">Sobre Nosotros</a></li>
-          <li><a href="/remeras-camisetas.html">Remeras y Camisetas</a></li>
-          <li><a href="/zapatillas.html">Zapatillas</a></li>
-          <li><a href="/pantalones.html">Pantalones</a></li>
+          <li><a href="remeras-camisetas.html">Remeras y Camisetas</a></li>
+          <li><a href="zapatillas.html">Zapatillas</a></li>
+          <li><a href="pantalones.html">Pantalones</a></li>
         </ul>
       </div>
       
       <div class="access-login">
         <ul>
-          <li><a href="/login.html"><i class="fa-solid fa-arrow-right-to-bracket"></i></a></li>
+          <li><a href="login.html"><i class="fa-solid fa-arrow-right-to-bracket"></i></a></li>
         </ul>
       </div>
     </nav>`;
@@ -31,17 +31,17 @@ footer.innerHTML = `
       <div class="tiendas">
         <h5>Locales</h5>
         <ul>
-          <li><a href="/locales.html">Nuestros Locales</a>
+          <li><a href="locales.html">Nuestros Locales</a>
           </li>
         </ul>
     </div>
     <div class="asistencia">
       <h5>Asistencia</h5>
       <ul>
-        <li><a href="/cambios-y-devoluciones.html">Cambios y Devoluciones</a></li>
-        <li><a href="/guia-talles.html">Guía de Talles</a></li>
-        <li><a href="/venta-mayorista.html">Venta Mayorista</a></li>
-        <li><a href="/formulario-contacto.html>Contacto</a></li>
+        <li><a href="cambios-y-devoluciones.html">Cambios y Devoluciones</a></li>
+        <li><a href="guia-talles.html">Guía de Talles</a></li>
+        <li><a href="venta-mayorista.html">Venta Mayorista</a></li>
+        <li><a href="formulario-contacto.html>Contacto</a></li>
       </ul>
     </div>
   </div>`;
