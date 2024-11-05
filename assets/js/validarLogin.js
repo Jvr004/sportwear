b0ce29329a872198ace1c9a6ca2870c18e20dde6
Jvr <jvr.t004@gmail.com>
2024-11-04 23:40:22 -0300

    Error Campo telefono---> solucionado

--- assets/js/validarLogin.js
+++ assets/js/validarLogin.js
@@ -1,3 +1,62 @@
+
+function validarLogin() {
+
+  const expresiones = {
+
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+
+    telefono: /^\d{10,14}$/ // 10 a 14 numeros.
+  }
+
+  const form = document.getElementById("formLogin");
+  const email = document.getElementById("login-email");
+  const password = document.getElementById("login-password");
+
+
+  // Variable para verificar si hay errores
+  var errores = false;
+
+  // Reiniciar mensajes de éxito y error
+
+
+  document.getElementById("login-email").style.display = "none";
+
+  document.getElementById("login-password").style.display = "none";
+
+
+  // Validar email
+  if (email.value.indexOf("@") === -1 || email.value.trim() === "") {
+    document.getElementById("login-email").style.display = "block";
+    errores = true;
+  }
+
+  // Validar password
+  if (password.value.trim() === "") {
+    document.getElementById("login-password").style.display = "block";
+    errores = true;
+  }
+
+  // Mostrar mensaje de éxito si no hay errores
+  if (!errores) {
+    document.getElementById("mensajeExitoso").style.display = "block";
+    mensajeExitoso.innerHTML =
+      `Te damos la bienvenida! 
+      `
+    form.reset();
+    return false; // Para evitar que se recargue la página, aunque sea un formulario de prueba
+  }
+
+  // Si hay errores, no enviar el formulario
+  return false;
+}
+
+
 function validarFormulario() {
 
   const expresiones = {
@@ -17,6 +76,7 @@ function validarFormulario() {
   const nombre = document.getElementById("nombre");
   const apellido = document.getElementById("apellido");
   const email = document.getElementById("user-email");
+  // const telefono = document.getElementById("tel");
   const password = document.getElementById("password");
   const mensajeExitoso = document.getElementById("mensajeExitoso");
 
@@ -33,6 +93,8 @@ function validarFormulario() {
   document.getElementById("apellidoError").style.display = "none";
 
   document.getElementById("emailError").style.display = "none";
+
+  // document.getElementById("telError").style.display = "none";
 
   document.getElementById("passwordError").style.display = "none";
 
@@ -53,6 +115,12 @@ function validarFormulario() {
     document.getElementById("emailError").style.display = "block";
     errores = true;
   }
+
+  // // Validar teléfono
+  // if (telefono.value.trim() === "") {
+  //   document.getElementById("telError").style.display = "block";
+  //   errores = true;
+  // }
 
   // Validar password
   if (password.value.trim() === "") {
